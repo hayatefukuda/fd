06bc5a5dc239cceb0646517aedf51c4e8d1fd448
hayatefukuda <hayate.fukuda@force-corp.co.jp>
2020-04-24 15:49:37 +0900

    2020-04-24

---
+++ js/jspractice.js
@@ -0,0 +1,49 @@
+// ocument.write("Hello World");
+// alert("Hello World");
+
+// console.log("Hello World!" + "田中さん");
+// console.log(10 + 10);
+// console.log("10" + "10");
+
+// console.log(1 + 1);
+// console.log(1 - 1);
+// console.log(2 * 2);
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+// let x = 2;
+// let y = 2;
+// x++;
+// console.log(x);
+// y--;
+// console.log(y);
+
+// let x = 7;
+// let y = x++;     // xの値がyに代入され、その後 x にインクリメント
+// let x2 = 7;
+// let y2 = ++x2;   // 先に x2 インクリメント、その後x2の値がy2に代入
+
+// console.log(x);
+// console.log(y);
+// console.log(x2);
+// console.log(y2);
+
+// console.log(x++);
+// console.log(++x);
+
+//変数とは、値を代入するための箱
+
+'use strict'
+
+let age = 30;
+
+//定数とは　値を別名で定義したもの
+
+const name = "田中太郎"; //letより使われがち
+
+age = age*2;
+
+console.log(`${name}さんの年齢は${age}です`);
+
+// ESG or ES2015
+// Var 変数名/定数 ←今ではほぼ使わない
